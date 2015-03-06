6ad291bdbe07a2a0e886e0e5dec7021fdfd34c08
Dominick Pham <dominick.pham@gmail.com>
2015-03-05 23:51:02 -0800

    cleanse

--- app.js
+++ app.js
@@ -42,17 +42,17 @@
      * Initialises WebGL and creates the 3D scene.
      */
     function loadScene() {
-        //    Get the canvas element
+        // Get the canvas element
         canvas = document.getElementById("webGLCanvas");
-        //    Get the WebGL context
+        // Get the WebGL context
         gl = canvas.getContext("experimental-webgl");
-        //    Check whether the WebGL context is available or not
-        //    if it's not available exit
+        // Check whether the WebGL context is available or not
+        // if it's not available exit
         if(!gl) {
             alert("There's no WebGL context available.");
             return;
         }
-        //    Set the viewport to the canvas width and height
+        // Set the viewport to the canvas width and height
         cw = window.innerWidth;
         ch = window.innerHeight;
         canvas.width = cw;
@@ -94,80 +94,79 @@
             gl.deleteProgram(fragmentShader);
             return;
         }
-        //    Install the program as part of the current rendering state
+        // Install the program as part of the current rendering state
         gl.useProgram(gl.program);
-        //    Get the vertexPosition attribute from the linked shader program
+        // Get the vertexPosition attribute from the linked shader program
         var vertexPosition = gl.getAttribLocation(gl.program, "vertexPosition");
-        //    Enable the vertexPosition vertex attribute array. If enabled, the array
-        //    will be accessed an used for rendering when calls are made to commands like
-        //    gl.drawArrays, gl.drawElements, etc.
+        // Enable the vertexPosition vertex attribute array. If enabled, the array
+        // will be accessed an used for rendering when calls are made to commands like
+        // gl.drawArrays, gl.drawElements, etc.
         gl.enableVertexAttribArray(vertexPosition);
 
-        //    Clear the color buffer (r, g, b, a) with the specified color
+        // Clear the color buffer (r, g, b, a) with the specified color
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        //    Clear the depth buffer. The value specified is clamped to the range [0,1].
-        //    More info about depth buffers: http://en.wikipedia.org/wiki/Depth_buffer
+        // Clear the depth buffer. The value specified is clamped to the range [0,1].
+        // More info about depth buffers: http://en.wikipedia.org/wiki/Depth_buffer
         gl.clearDepth(1.0);
-        //    Enable depth testing. This is a technique used for hidden surface removal.
-        //    It assigns a value (z) to each pixel that represents the distance from this
-        //    pixel to the viewer. When another pixel is drawn at the same location the z
-        //    values are compared in order to determine which pixel should be drawn.
-        //gl.enable(gl.DEPTH_TEST);
+        // Enable depth testing. This is a technique used for hidden surface removal.
+        // It assigns a value (z) to each pixel that represents the distance from this
+        // pixel to the viewer. When another pixel is drawn at the same location the z
+        // values are compared in order to determine which pixel should be drawn.
+        // gl.enable(gl.DEPTH_TEST);
         gl.enable(gl.BLEND);
         gl.disable(gl.DEPTH_TEST);
         gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-        //    Specify which function to use for depth buffer comparisons. It compares the
-        //    value of the incoming pixel against the one stored in the depth buffer.
-        //    Possible values are (from the OpenGL documentation):
-        //    GL_NEVER - Never passes.
-        //    GL_LESS - Passes if the incoming depth value is less than the stored depth value.
-        //    GL_EQUAL - Passes if the incoming depth value is equal to the stored depth value.
-        //    GL_LEQUAL - Passes if the incoming depth value is less than or equal to the stored depth value.
-        //    GL_GREATER - Passes if the incoming depth value is greater than the stored depth value.
-        //    GL_NOTEQUAL - Passes if the incoming depth value is not equal to the stored depth value.
-        //    GL_GEQUAL - Passes if the incoming depth value is greater than or equal to the stored depth value.
-        //    GL_ALWAYS - Always passes.
-        //gl.depthFunc(gl.LEQUAL);
+        // Specify which function to use for depth buffer comparisons. It compares the
+        // value of the incoming pixel against the one stored in the depth buffer.
+        // Possible values are (from the OpenGL documentation):
+        // GL_NEVER - Never passes.
+        // GL_LESS - Passes if the incoming depth value is less than the stored depth value.
+        // GL_EQUAL - Passes if the incoming depth value is equal to the stored depth value.
+        // GL_LEQUAL - Passes if the incoming depth value is less than or equal to the stored depth value.
+        // GL_GREATER - Passes if the incoming depth value is greater than the stored depth value.
+        // GL_NOTEQUAL - Passes if the incoming depth value is not equal to the stored depth value.
+        // GL_GEQUAL - Passes if the incoming depth value is greater than or equal to the stored depth value.
+        // GL_ALWAYS - Always passes.
+        // gl.depthFunc(gl.LEQUAL);
 
-        //    Now create a shape.
-        //    First create a vertex buffer in which we can store our data.
+        // Now create a shape.
+        // First create a vertex buffer in which we can store our data.
         var vertexBuffer = gl.createBuffer();
-        //    Bind the buffer object to the ARRAY_BUFFER target.
+        // Bind the buffer object to the ARRAY_BUFFER target.
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-        //    Specify the vertex positions (x, y, z)
+        // Specify the vertex positions (x, y, z)
         vertices = [];
         ratio = cw / ch;
         velocities = [];
-        for ( var i=0; i<numLines; i++ )
-        {
+        for(var i = 0; i < numLines; i++) {
             vertices.push( 0, 0, 1.83 );//(Math.random() * 2 - 1)*ratio, Math.random() * 2 - 1, 1.83 );
             velocities.push( (Math.random() * 2 - 1)*0.05, (Math.random() * 2 - 1)*0.05, 0.93 + Math.random()*0.02 );
         }
         vertices = new Float32Array( vertices );
         velocities = new Float32Array( velocities );
 
-        //    Creates a new data store for the vertices array which is bound to the ARRAY_BUFFER.
-        //    The third paramater indicates the usage pattern of the data store. Possible values are
-        //    (from the OpenGL documentation):
-        //    The frequency of access may be one of these:
-        //    STREAM - The data store contents will be modified once and used at most a few times.
-        //    STATIC - The data store contents will be modified once and used many times.
-        //    DYNAMIC - The data store contents will be modified repeatedly and used many times.
-        //    The nature of access may be one of these:
-        //    DRAW - The data store contents are modified by the application, and used as the source for
-        //           GL drawing and image specification commands.
-        //    READ - The data store contents are modified by reading data from the GL, and used to return
-        //           that data when queried by the application.
-        //    COPY - The data store contents are modified by reading data from the GL, and used as the source
-        //           for GL drawing and image specification commands.
+        // Creates a new data store for the vertices array which is bound to the ARRAY_BUFFER.
+        // The third paramater indicates the usage pattern of the data store. Possible values are
+        // (from the OpenGL documentation):
+        // The frequency of access may be one of these:
+        // STREAM - The data store contents will be modified once and used at most a few times.
+        // STATIC - The data store contents will be modified once and used many times.
+        // DYNAMIC - The data store contents will be modified repeatedly and used many times.
+        // The nature of access may be one of these:
+        // DRAW - The data store contents are modified by the application, and used as the source for
+        //        GL drawing and image specification commands.
+        // READ - The data store contents are modified by reading data from the GL, and used to return
+        //        that data when queried by the application.
+        // COPY - The data store contents are modified by reading data from the GL, and used as the source
+        //        for GL drawing and image specification commands.
         gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW);
 
-        //    Clear the color buffer and the depth buffer
+        // Clear the color buffer and the depth buffer
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        //    Define the viewing frustum parameters
-        //    More info: http://en.wikipedia.org/wiki/Viewing_frustum
-        //    More info: http://knol.google.com/k/view-frustum
+        // Define the viewing frustum parameters
+        // More info: http://en.wikipedia.org/wiki/Viewing_frustum
+        // More info: http://knol.google.com/k/view-frustum
         var fieldOfView = 30.0;
         var aspectRatio = canvas.width / canvas.height;
         var nearPlane = 1.0;
@@ -177,9 +176,9 @@
         var right = top * aspectRatio;
         var left = -right;
 
-        //     Create the perspective matrix. The OpenGL function that's normally used for this,
-        //     glFrustum() is not included in the WebGL API. That's why we have to do it manually here.
-        //     More info: http://www.cs.utk.edu/~vose/c-stuff/opengl/glFrustum.html
+        // Create the perspective matrix. The OpenGL function that's normally used for this,
+        // glFrustum() is not included in the WebGL API. That's why we have to do it manually here.
+        // More info: http://www.cs.utk.edu/~vose/c-stuff/opengl/glFrustum.html
         var a = (right + left) / (right - left);
         var b = (top + bottom) / (top - bottom);
         var c = (farPlane + nearPlane) / (farPlane - nearPlane);
@@ -193,40 +192,40 @@
             0, 0, -1, 0
         ];
 
-        //     Create the modelview matrix
-        //     More info about the modelview matrix: http://3dengine.org/Modelview_matrix
-        //     More info about the identity matrix: http://en.wikipedia.org/wiki/Identity_matrix
+        // Create the modelview matrix
+        // More info about the modelview matrix: http://3dengine.org/Modelview_matrix
+        // More info about the identity matrix: http://en.wikipedia.org/wiki/Identity_matrix
         var modelViewMatrix = [
             1, 0, 0, 0,
             0, 1, 0, 0,
             0, 0, 1, 0,
             0, 0, 0, 1
         ];
-        //     Get the vertex position attribute location from the shader program
+        // Get the vertex position attribute location from the shader program
         var vertexPosAttribLocation = gl.getAttribLocation(gl.program, "vertexPosition");
-        //				colorLoc = gl.getVaryingLocation(gl.program, "vColor");
-        //				alert("color loc : " + colorLoc );
-        //     Specify the location and format of the vertex position attribute
+        // colorLoc = gl.getVaryingLocation(gl.program, "vColor");
+        // alert("color loc : " + colorLoc );
+        // Specify the location and format of the vertex position attribute
         gl.vertexAttribPointer(vertexPosAttribLocation, 3.0, gl.FLOAT, false, 0, 0);
-        //gl.vertexAttribPointer(colorLoc, 4.0, gl.FLOAT, false, 0, 0);
-        //     Get the location of the "modelViewMatrix" uniform variable from the
-        //     shader program
+        // gl.vertexAttribPointer(colorLoc, 4.0, gl.FLOAT, false, 0, 0);
+        // Get the location of the "modelViewMatrix" uniform variable from the
+        // shader program
         var uModelViewMatrix = gl.getUniformLocation(gl.program, "modelViewMatrix");
-        //     Get the location of the "perspectiveMatrix" uniform variable from the
-        //     shader program
+        // Get the location of the "perspectiveMatrix" uniform variable from the
+        // shader program
         var uPerspectiveMatrix = gl.getUniformLocation(gl.program, "perspectiveMatrix");
         //     Set the values
         gl.uniformMatrix4fv(uModelViewMatrix, false, new Float32Array(perspectiveMatrix));
         gl.uniformMatrix4fv(uPerspectiveMatrix, false, new Float32Array(modelViewMatrix));
-        //	gl.varyingVector4fv(
-        //     Draw the triangles in the vertex buffer. The first parameter specifies what
-        //     drawing mode to use. This can be GL_POINTS, GL_LINE_STRIP, GL_LINE_LOOP,
-        //     GL_LINES, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN, GL_TRIANGLES, GL_QUAD_STRIP,
-        //     GL_QUADS, and GL_POLYGON
-        //gl.drawArrays( gl.LINES, 0, numLines );
-        //gl.flush();
+        // gl.varyingVector4fv(
+        // Draw the triangles in the vertex buffer. The first parameter specifies what
+        // drawing mode to use. This can be GL_POINTS, GL_LINE_STRIP, GL_LINE_LOOP,
+        // GL_LINES, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN, GL_TRIANGLES, GL_QUAD_STRIP,
+        // GL_QUADS, and GL_POLYGON
+        // gl.drawArrays( gl.LINES, 0, numLines );
+        // gl.flush();
 
-        //setInterval( drawScene, 1000 / 40 );
+        // setInterval( drawScene, 1000 / 40 );
         animate();
     }
 
@@ -299,9 +298,9 @@
 
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        //gl.drawArrays( gl.LINES_STRIP, 0, numLines );
-        gl.drawArrays( gl.LINES, 0, numLines );
-        //gl.drawArrays( gl.QUAD_STRIP, 0, numLines );
+        // gl.drawArrays(gl.LINES_STRIP, 0, numLines);
+        gl.drawArrays(gl.LINES, 0, numLines);
+        // gl.drawArrays(gl.QUAD_STRIP, 0, numLines);
 
         gl.flush();
     }
